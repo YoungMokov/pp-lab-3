3697553b74b6ee487b351c8f43181dae6189a842
Mikołaj Romanowski <84807601+YoungMokov@users.noreply.github.com>
2024-04-06 08:59:01 +0200

    Add files via upload

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+
+    public static void main(String[] args) {
+        Point punkt = new Point(2, 3);
+
+        System.out.println("Wartosci punktu:");
+        System.out.println("x: " + punkt.getX());
+        System.out.println("y: " + punkt.getY());
+    }
+}
